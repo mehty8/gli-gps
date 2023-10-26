6845d04ec47c97912b6cc85ddc4f930ffe4020de
Csanyi Balint <113693078+mehty8@users.noreply.github.com>
2023-10-26 18:34:42 +0200

    create HandleInputExtension class to check the provided and the requested json file path and the inserted unit type validity as well

---
+++ Input/HandleInputExtension.cs
@@ -0,0 +1,90 @@
+﻿using System.Text.RegularExpressions;
+
+namespace gli_gps.Input;
+
+public static class HandleInputExtension
+{
+    public static bool CheckIncomingFilePath(this string input)
+    {
+        if (input.Length == 0)
+        {
+            Console.WriteLine("You did not provide any path. Please try again");
+            return false;
+        }
+        bool regex = Regex.IsMatch(input.ToLower(), "\\.json$");
+        if (!regex)
+        {
+            Console.WriteLine("You must have mistyped something, the path must end with .json. Please try again:");
+            return false;
+        }
+
+        if (!File.Exists(input))
+        {
+            Console.WriteLine("You must have mistyped something, " +
+                              "at the provided path there is no such a json file. Please try again:");
+            return false;
+        }
+        return true;
+    }
+
+    public static bool CheckOutgoingFilePath(this string input)
+    {
+        if (input.Length == 0)
+        {
+            Console.WriteLine("You did not provide any path. Please try again");
+            return false;
+        }
+        bool regex = Regex.IsMatch(input.ToLower(), "\\.json$");
+        if (!regex)
+        {
+            Console.WriteLine("You must have mistyped something, the path must end with .json. Please try again:");
+            return false;
+        }
+
+        int endOfDirectoryIndex = input.LastIndexOf("/");
+        if (endOfDirectoryIndex == -1)
+        {
+            Console.WriteLine("You must have mistyped something, A correct path has to contain '/'. Please try again:");
+            return false;
+        }
+        
+        string outputDirectory = input.Substring(0,input.LastIndexOf("/"));
+        if (!Directory.Exists(outputDirectory))
+        {
+            Console.WriteLine("You must have mistyped something, " +
+                              "the provided directory does not exist. Please try again:");
+            return false;
+        }
+        return true;
+    }
+
+    public static bool CheckUnitType(this string input)
+    {
+        if (input.Length == 0)
+        {
+            Console.WriteLine("You did not provide any path. Please try again");
+            return false;
+        }
+
+        bool regex = Regex.IsMatch(input.ToLower(), "metric|imperial");
+        if (!regex)
+        {
+            Console.WriteLine("You must have mistyped something, please type 'metric' " +
+                              "if You want the distance in metres and the bearing in degree,\n or type 'imperial'," +
+                              "If You want the distance in feet and the bearing in radian:");
+            return false;
+        }
+
+        int metric = input.ToLower().IndexOf("metric");
+        int imperial = input.ToLower().IndexOf("imperial");
+       
+        if (metric >= 0 && imperial >= 0)
+        {
+            Console.WriteLine("You must have mistyped, You provided both 'metric' and 'imperial'. " +
+                              "Please choose only one:");
+            return false;
+        }
+
+        return true;
+    }
+}
