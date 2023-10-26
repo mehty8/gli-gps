ffe7f45af0bb6364f24d486b63fb29fb5351a387
Csanyi Balint <113693078+mehty8@users.noreply.github.com>
2023-10-26 18:37:12 +0200

    create the input handling interface and its implementing class to receive input then check them by the extension class methods to return only valid inputs

---
+++ Input/HandleInput.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.RegularExpressions;
+
+namespace gli_gps.Input;
+
+public class HandleInput : IHandleInput
+{
+    public string GetIncomingFilePath(string message)
+    {
+        Console.WriteLine(message);
+        string input = Console.ReadLine();
+        while (!input.CheckIncomingFilePath())
+        {
+            input = Console.ReadLine();
+        }
+
+        return input;
+    }
+
+    public string GetOutgoingFilePath(string message)
+    {
+        Console.WriteLine(message);
+        string input = Console.ReadLine();
+        while (!input.CheckOutgoingFilePath())
+        {
+            input = Console.ReadLine();
+        }
+
+        return input;
+    }
+
+    public string GetUnitType(string message)
+    {
+        Console.WriteLine(message);
+        string input = Console.ReadLine();
+        while (!input.CheckUnitType())
+        {
+            input = Console.ReadLine();
+        }
+
+        return Regex.Replace(input.ToLower(), ".*?(metric|imperial).*", "$1");
+    }
+}
---
+++ Input/IHandleInput.cs
@@ -0,0 +1,10 @@
+﻿namespace gli_gps.Input;
+
+public interface IHandleInput
+{
+    string GetIncomingFilePath(string message);
+
+    string GetOutgoingFilePath(string message);
+
+    string GetUnitType(string message);
+}
