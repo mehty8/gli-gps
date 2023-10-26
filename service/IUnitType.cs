9af87142776002d39bc559b4e01e589d35bcd139
Csanyi Balint <113693078+mehty8@users.noreply.github.com>
2023-10-26 18:41:25 +0200

    create interface of different unit types

---
+++ service/IUnitType.cs
@@ -0,0 +1,8 @@
+﻿namespace gli_gps.service;
+
+public interface IUnitType
+{
+    public bool IsRequestedType(string unitType);
+
+    public Dictionary<string, int> Calculate(double lat1, double lat2, double lon1, double lon2);
+}
