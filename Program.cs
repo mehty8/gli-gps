301d856d09dfaa9be75c4bbe65e19b98f541d748
Csanyi Balint <113693078+mehty8@users.noreply.github.com>
2023-10-26 19:16:02 +0200

    create the main method in program class, instantiating the necessary classes and calling the required methods within to run the app. App is complete.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using gli_gps.Input;
+using gli_gps.service;
 
-Console.WriteLine("Testing gitHub");
+void Main()
+{
+    IHandleInput handleInput = new HandleInput();
+    string incomingFilePath = handleInput.GetIncomingFilePath("Please provide the entire path of the json file\n" +
+                                                              "(example:/mnt/c/yourDirectory/yourJsonFile.json):");
+    
+    string outgoingFilePath = handleInput.GetOutgoingFilePath("Please provide the entire directory along with" +
+                                                              " the file's name to save the output json file\n" +
+                                                              "(example:/mnt/c/yourDirectory/newJsonFile.json):");
+    
+    string unit = handleInput.GetUnitType("Please type 'metric' if You want the distance in metres and the bearing" +
+                                          " in degree,\nor type 'imperial', if You want the distance in feet and" +
+                                          " the bearing in radian:");
+    
+    List<IUnitType> unitTypes = new() { new UnitTypeMetric(), new UnitTypeImperial() };
+    CreateJson createJson = new CreateJson(unitTypes);
+    createJson.WriteJson(incomingFilePath, outgoingFilePath, unit);
+}
+Main();
